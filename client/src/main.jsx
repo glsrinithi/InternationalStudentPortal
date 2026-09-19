6ebc1961b07b5c5ce314126c6c672a45504569a5
Glsrinithi <glsrinithi@gmail.com>
2026-09-19 20:53:26 +0530

    Fix GitHub Pages routing

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -8,7 +8,7 @@ import './styles/global.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/InternationalStudentPortal">
       <ToastProvider>
         <AuthProvider>
           <App />
